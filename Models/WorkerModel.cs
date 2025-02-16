5dc47ca8ceac5048d47d8d7266806d9ed89002dd
Stanisław Franczak <s.franczak20@gmail.com>
2025-02-16 22:51:53 +0100

    Basic connection achieved (workers)

---
+++ Models/WorkerModel.cs
@@ -0,0 +1,15 @@
+﻿namespace MSOI.Models
+{
+    public class WorkerModel
+    {
+
+        public int Id { get; set; }
+        public string Worker_name { get; set; } = string.Empty;
+        public string Worker_surname { get; set; } = string.Empty;
+        public string Position { get; set; } = string.Empty;
+        public DateTime Employment_date { get; set; }
+        public string Pesel { get; set; } = string.Empty;
+
+
+    }
+}
