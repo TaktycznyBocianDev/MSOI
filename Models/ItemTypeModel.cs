5882bffd8a8764ce2d95d0e4831ad6ce4adce680
Stanisław Franczak <s.franczak20@gmail.com>
2025-02-26 23:40:14 +0100

    Bug fixing

--- Models/ItemTypeModel.cs
+++ Models/ItemTypeModel.cs
@@ -2,7 +2,6 @@
 {
     public class ItemTypeModel
     {
-
         public int Id { get; set; }
         public string Item_name { get; set; } = string.Empty;
         public int Default_replacement_period { get; set; }
