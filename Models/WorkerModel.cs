9b1203bbf788e3cb37952d90cb5bbae53695864c
Stanisław Franczak <s.franczak20@gmail.com>
2025-04-16 00:06:42 +0200

    Adjusting workers page

--- Models/WorkerModel.cs
+++ Models/WorkerModel.cs
@@ -7,7 +7,7 @@
         public string Worker_name { get; set; } = string.Empty;
         public string Worker_surname { get; set; } = string.Empty;
         public string Position { get; set; } = string.Empty;
-        public DateTime Employment_date { get; set; }
+        public DateTime? Employment_date { get; set; }
         public string Pesel { get; set; } = string.Empty;
 
 
